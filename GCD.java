11611b64a36e43af3cfdb9a7bb009badb698ff60
vmdharan <vmdharan@users.noreply.github.com>
2019-12-10 20:11:01 +1030

    Added comment and extra test cases.

--- GCD.java
+++ GCD.java
@@ -1,8 +1,18 @@
+/**
+ * Calculate the Greatest Common Divisor (GCD) using the 
+ * Iterative Euclidean algorithm. 
+ */
 public class GCD {
     public static void main(String[] args)
     {
         int gcd = CalculateGCD(128, 48);
         System.out.println(gcd);
+
+        int gcd2 = CalculateGCD(1024, 320);
+        System.out.println(gcd2);
+
+        int gcd3 = CalculateGCD(784, 4662);
+        System.out.println(gcd3);
     }
 
     public static int CalculateGCD(int n1, int n2)
